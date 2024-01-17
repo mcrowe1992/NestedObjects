53561bc7666ed934f860f4694b8b6fac262c3ed8
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-01-16 23:00:29 -0800

    Created student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+	/// <summary>
+	/// Represents a colege advisor
+	/// </summary>
+	public class Advisor
+	{
+		/// <summary>
+		/// The legal first and last name
+		/// </summary>
+		public string FullName { get; set; }
+
+		/// <summary>
+		/// The advisors work email
+		/// </summary>
+		public string Email { get; set; }
+
+		/// <summary>
+		/// Building and room number of the office
+		/// </summary>
+		public string OfficeLocation { get; set; }
+
+	}
+}
